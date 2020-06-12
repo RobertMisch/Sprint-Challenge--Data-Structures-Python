1f2fc9553985ddbf1abb9685cfa615a6e80da057
RobertMisch <robertmisch94@gmail.com>
2020-06-12 13:31:22 -0400

    implimented nbames stretch in O(n)

--- names/names.py
+++ names/names.py
@@ -76,16 +76,19 @@ for name in names_2:
     if myTree.contains(name):
         duplicates.append(name)
 
-end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
 # What's the best time you can accomplish?  Thare are no restrictions on techniques or data
 # structures, but you may not import any additional libraries that you did not write yourself.
+#using a set brings it down to O(n)
+# a=set(names_1)
+# b=set(names_2)
+# print(a&b)
 
-
+end_time = time.time()
+print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print (f"runtime: {end_time - start_time} seconds")
 ########################################
 # BST
 # import collections #get the deque from python
